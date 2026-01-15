84adf06d09413f85ee41883c01dcd3a36aa537d4
Kovaci Kostanca <kovacikostanca@gmail.com>
2026-01-15 10:17:09 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const serviceCards = document.querySelectorAll('.service-card');
 serviceCards.forEach((card, index) => {
   card.addEventListener('click', () => {
     // Open the corresponding service page in the ../services/ folder
-    window.location.href = `../services/service${index + 1}.html`;
+    window.location.href = `services/service${index + 1}.html`;
   });
 });
 
